1947e6c03ca25b88454d3488e03cb3590873b723
LionelKarlen <lionel.karlen@protonmail.com>
2023-10-09 00:23:26 +0200

    ci: Add deploy workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -15,7 +17,10 @@ const config = {
 			assets: 'build',
 			pages: 'build',
 			fallback: 'index.html'
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/doomsday'
+		}
 	}
 };
 
